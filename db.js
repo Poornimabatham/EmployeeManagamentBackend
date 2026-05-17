b0669c0f45dd92b97a11d057dc5255029e2a6825
Poornima Batham <pbatham21@gmail.com>
2026-05-17 14:49:35 +0530

    add code

--- db.js
+++ db.js
@@ -1,17 +1,20 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) throw err;
   console.log("MySQL connected");
+  connection.release();
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -8,4 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
