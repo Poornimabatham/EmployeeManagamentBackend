e378022f4d20b41cdc7d36ce6948c456bab415a6
Poornima Batham <pbatham21@gmail.com>
2026-05-17 19:14:33 +0530

    add code

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controller/authController');
+const authController = require("../controller/authController");
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 router.post("/send-admin-code", authController.sendAdminCode);
 
 module.exports = router;
